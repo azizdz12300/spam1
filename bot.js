bd6ad7ba5301c02952ea4395945f355cdcabd104
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-04-03 20:44:24 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,8 +8,8 @@ other.on('ready', () => {
 other.on('ready', async () => {
       let ReBeL = ["**كس امك ياكليوا يبن القحبا ادمرك يا ابن شرموطا لاتغلط معي ادمرك حتشوف يعهرا مسوي نفسك مو متكبر**"]
   setInterval(() => {
-other.channels.get("").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+other.channels.get("559721071398617102").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
 },300);
 });
 
-other.login("NTYyMzYzOTM4ODYwMzAyMzQ1.XKNp-A.g6UfqB6W9dNs04QuvtueLnIiRE4")
+other.login("NTQxMjg1MTg0ODE4MTE4NjU2.XKULqQ.xn8gDfDD3D2bHFZLrywRb51vFHM")
