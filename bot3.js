786aad3bf080cc061e9fc80001e569b6e81b1e70
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-06-21 20:04:11 +0100

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -12,4 +12,4 @@ other.channels.get("591701938635276300").send(`${ReBeL[Math.floor(Math.random() 
 },300);
 });
 
-other.login("NTExNjM2MTEzMzg4MDc3MDc3.DtHXWg.3XBZZ9QCfrFoVszcGavPh7nGI1c")
+other.login("MzkwOTg2MTEzNzc2NjE1NDM4.XQ0p0A.GkHOExSSLRxMLvM0hrTuSoZW1jc")
