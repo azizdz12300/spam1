1373e2594f7a5965bfd9e3b130471d8c332c8074
azizdz12300 <44880483+azizdz12300@users.noreply.github.com>
2019-06-21 20:03:27 +0100

    Update bot3.js

--- bot3.js
+++ bot3.js
@@ -8,7 +8,7 @@ other.on('ready', () => {
 other.on('ready', async () => {
       let ReBeL = ["هلا بلزين تراني بوت تبند اشيلك","هي انا بولعها كريديتس لوووووول","Hi Im Credits Agaaaaaaaaain"]
   setInterval(() => {
-other.channels.get("513395898840121395").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
+other.channels.get("591701938635276300").send(`${ReBeL[Math.floor(Math.random() *ReBeL.length)]}`);//لا تنسى تحط أيدي الروم
 },300);
 });
 
